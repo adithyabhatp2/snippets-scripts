be17d124396bfe222d0ffc272d226124c5ba792b
adithyabhatp2 <adithyabhatp2@github.com>
2016-10-23 22:41:48 -0500

    grading helper - fill report from local files

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # snippets-scripts
 Scripts and Snippets of code that may be (re)useful
+
+## canvas.wisc grading helper
+student submissions (graded) are local file disks
+form entry on canvas.wisc
+jQuery is imported in the canvas speedgrader tool/page
+python code to expose local files via http/https to pull from the browser on canvas via jquery
+
+run python web server to make student grading files available.. https probably necessary
+
+C:\Users\Elcot\AppData\Local\Google\Chrome\Application\chrome.exe --user-data-dir="C:/Chrome dev session" --disable-web-security
+
+goto https://localhost:4443 , accept the certificate, verify you can see the files.
+
+use google chrome (or FF), and get the console (easy way is to inspect element and switch tab)
+paste the below in the console, and call function..
+or just paste the piece of code you want to use.
+
---
+++ grading_helper.js
@@ -0,0 +1,57 @@
+
+function doGrading() {
+
+
+var name = $('.ui-selectmenu-item-header')[0].innerHTML.replace('-','')
+var subName = name.split(' ')[1].toLowerCase()+name.split(' ')[0].toLowerCase();
+var scoreData;
+
+$.get('//localhost:4443/results/'+subName+'/grade_report.txt', function(data) { 
+scoreData = data; 
+
+var commentArea=$('#speedgrader_comment_textarea');
+commentArea.val(scoreData);
+
+var submitBtn = $('#comment_submit_button');
+submitBtn.click();
+
+var rubricBtn = $('.toggle_full_rubric.edit.btn');
+rubricBtn.click();
+
+var meGrader = $('#rating_970_1872');
+meGrader.click();
+
+var fullMarks = $('#rating_970_960');
+fullMarks.click();
+
+var goodStyle = $('#rating_970_9715');
+goodStyle.click();
+
+var score = scoreData.split('Total: ')[1].split(' / ')[0];
+
+var points = $('td.nobr.points_form > div.editing > span > input').first();
+points.val(parseInt(score)+5);
+
+var saveRubricBtn = $('.save_rubric_button');
+saveRubricBtn.focus();
+
+});
+
+var saveRubricBtn = $('.save_rubric_button');
+saveRubricBtn.focus();
+
+clear(); // chrome / FF console clearing
+
+var saveRubricBtn = $('.save_rubric_button');
+saveRubricBtn.focus();
+
+subName;
+
+}
+
+
+
+
+
+
+
